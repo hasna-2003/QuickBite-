93a37f38923af4a9c729276d2a9ec386067587eb
hasna-2003 <hasnamass2003@gmail.com>
2026-06-20 22:56:05 +0530

    Add folder to admin-panel

---
+++ adminpanel/src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import Sidebar from './components/Sidebar/Sidebar';
+import Menubar from './components/Menubar/Menubar';
+import { Routes, Route } from 'react-router-dom';
+import AddFood from './pages/AddFood/AddFood';
+import ListFood from './pages/ListFood/ListFood';
+import Order from './pages/Order/Order';
+
+const App = () => {
+  const [sidebarVisible , setSidebarVisible] = useState(true);
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!sidebarVisible);
+  }
+
+  return (
+            <div className="d-flex" id="wrapper">
+
+
+            <Sidebar sidebarVisible={sidebarVisible} />
+           
+            <div id="page-content-wrapper">
+
+              <Menubar toggleSidebar={toggleSidebar}  />
+              
+                <div className="container-fluid">
+                  <Routes>
+                    <Route path="/addfood" element={<AddFood />} />
+                    <Route path="/listfood" element={<ListFood />} />
+                    <Route path="/order" element={<Order />} />
+                      <Route path="/" element={<ListFood />} />
+                  </Routes>
+                </div>
+        </div>
+    </div>
+  )
+}
+
+export default App
